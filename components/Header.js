89f092d1f1e4292b56bc9849a31abbc87dec852c
johannkirr <114095519+johannkirr@users.noreply.github.com>
2023-01-08 10:23:32 +0100

    add the menu and profile Img to right side of header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,19 @@
 import Image from "next/image"
 import React from "react"
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid"
 
 export default function Header() {
   return (
   
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid hover:scale-125 transision-transform duration-200 ease-out">
             <Image src="/../public/bilder/logo.jpg" 
                    layout='fill'
                    className="object-contain">
             </Image>
         </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden hover:scale-125 transision-transform duration-200 ease-out">
             <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
                    layout='fill'
                    className="object-contain">
@@ -25,7 +26,11 @@ export default function Header() {
           <input className="bg-gray-100 pl-10 border-transparent focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search"></input>
         </div>
 
-        <h1>Right sides</h1>
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transision-transform duration-200 ease-out"/>
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transision-transform duration-200 ease-out"/>
+          <Image className="rounded-full cursor-pointer hover:scale-125 transision-transform duration-200 ease-out" src="/../public/bilder/foto.jpg" width={20} height={20} alt="user-image"/>
+        </div>
 
       </div>
    
